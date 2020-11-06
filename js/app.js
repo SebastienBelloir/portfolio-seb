35e1a05a4063862d1b5325e4eeeda80892c8ca05
Belloir Sébastien <sebastien.belloir@gmail.com>
2020-11-06 16:34:42 +0100

    animations panel

--- js/app.js
+++ js/app.js
@@ -21,14 +21,14 @@ tl2.to(".box-top-left", { duration: 1, width: "80%", delay: 0.5})
 
 const words = ["DÉVELOPPEUR WEB", "FRONT-END", "AIME RELEVER", "DES DÉFIS"];
 
-const cursor = gsap.to(".cursor", {opacity: 0, ease:"power2.inOut", repeat:-1, duration:1});
+const cursor = gsap.to(".cursor2", {opacity: 0, ease:"power2.inOut", repeat:-1, duration:1});
 
 const boxTl = gsap.timeline();
 
 boxTl.to(".box", {duration: 1, width: "23vw", delay: 0.5, ease: "power4.inOut"})
     .from(".name", {opacity: 0, duration: 1, y: "7vw", ease: "power3.out", onComplete: () => masterTl.play()})
     .from(".is", {opacity: 0, duration: 1, y: "7vw", ease: "power3.out"})
-    .from(".cursor", {duration: 1, y: "7vw", ease: "power3.out"})
+    .from(".cursor2", {duration: 1, y: "7vw", ease: "power3.out"})
     .to(".box", {duration: 1.2, height: "7vw", x: -50, ease: "elastic.out"})
     .to(".box", {duration: 2, autoAlpha:0.5, yoyo:true, repeat: -1, ease:"rough({ template: none.out, strength:  1, points: 20, taper: 'none', randomize: true, clamp: false})"})
 
@@ -48,7 +48,6 @@ tl3.from(".profil-picture", {
         start: "top center",
         end: "50% top",
         scrub: true,
-        markers: true
     },
         x: -500,
         ease: "power4.out",
@@ -60,41 +59,66 @@ tl3.from(".profil-picture", {
         start: "top center",
         end: "top top",
         scrub: true,
-        markers: true
     },
         x: 700,
         y: 700,
         ease: "power4.out",
         duration: 4
 })
-
     .to(".about-me", {
     scrollTrigger:{
         trigger: "#about",
         start: "top center",
         end: "top top",
         scrub: true,
-        markers: true
+        
     },
         rotation: "-90deg",
+        color: "rgba(0, 0, 255, 0.658)",
         ease: "power4.out",
         duration: 4
 })
+
+
 //Using timeline from GSAP to move boxes out of screen for better animation during scroll. 
 const tl4 = gsap.timeline();
-    tl4.from("#about", {xPercent: -100})
-   
+    tl4.fromTo("#skills", {xPercent: -120, background: "rgba(0, 128, 0, 0.658)"}, {xPercent: 0, background: "rgb(0, 128, 0)"})
+    tl4.fromTo("#projects", {xPercent: 100, background: "rgba(255, 255, 255, 0.658)"}, {delay: 0.3,xPercent: 0, background: "#FFF"})
 
 
- //Using scrolltrigger from GSAP¨to trigger animation on scroll from #container.   
+ //Using scrolltrigger from GSAP to trigger animation on scroll from #container.   
 ScrollTrigger.create({
     animation: tl4,
     trigger: "#about",
     start: "top top",
-    end: "+=4000",
-    markers: true,
-    scrub: 1,
+    end: "+=2000",
+    scrub: true,
     pin: true,
-    pinSpacing: false,
     anticipatePin: 1
+})
+
+//cursor modification
+const cursor1 = document.querySelector(".cursor");
+
+document.addEventListener('mousemove', e => {
+    cursor1.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+})
+
+document.addEventListener('click', () => {
+    cursor1.classList.add('expand');
+
+    setTimeout(() => {
+        cursor1.classList.remove('expand');
+    }, 500)
+})
+// logos' animation
+
+gsap.from(".html5", {
+    scrollTrigger: ".html5",
+    x: 700,
+    rotation: 360,
+    delay: 2,
+    start: "top top",
+    markers: true,
+    toggleActions: "restart reverse reverse pause"
 })
